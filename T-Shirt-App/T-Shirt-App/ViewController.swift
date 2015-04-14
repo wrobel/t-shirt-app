f76b0a328c5541913bd3e5eef81662e1c2f31fdc
Gunnar Wrobel <p@rdus.de>
2015-04-14 18:15:45 +0200

    Initial commit

---
+++ T-Shirt-App/T-Shirt-App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  T-Shirt-App
+//
+//  Created by Gunnar Wrobel on 14.04.15.
+//  Copyright (c) 2015 Gunnar Wrobel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
