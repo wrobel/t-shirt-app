091d7a3720bb1c0119bca7e3a6e76c96798160fb
tdeekens <tobias.deekens@epost-dev.de>
2015-04-22 21:47:29 +0200

    Add initial payment screen gathering creditcard details.

---
+++ T-Shirt-App/T-Shirt-App-Swift-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  T-Shirt-App-Swift-Bridging-Header.h
+//  T-Shirt-App
+//
+//  Created by Tobias Deekens on 22/04/15.
+//  Copyright (c) 2015 Gunnar Wrobel. All rights reserved.
+//
+
+#ifndef T_Shirt_App_T_Shirt_App_Swift_Bridging_Header_h
+#define T_Shirt_App_T_Shirt_App_Swift_Bridging_Header_h
+
+#import "CardIO.h"
+
+#endif
---
+++ T-Shirt-App/T-Shirt-App/PaymentViewController.swift
@@ -0,0 +1,59 @@
+//
+//  PaymentViewController.swift
+//  T-Shirt-App
+//
+//  Created by Tobias Deekens on 22/04/15.
+//  Copyright (c) 2015 Gunnar Wrobel. All rights reserved.
+//
+
+import UIKit
+
+class PaymentViewController: UIViewController, UINavigationControllerDelegate, CardIOPaymentViewControllerDelegate {
+    
+    @IBOutlet var statusLabel:UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        CardIOUtilities.preload()
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func scanCard(sender: AnyObject) {
+        var cardIOVC = CardIOPaymentViewController(paymentDelegate: self)
+        cardIOVC.modalPresentationStyle = .FormSheet
+        presentViewController(cardIOVC, animated: true, completion: nil)
+    }
+    
+    func userDidCancelPaymentViewController(paymentViewController: CardIOPaymentViewController!) {
+        statusLabel.text = "Die Zahlungdateneingabe wurde abgebrochen."
+        paymentViewController?.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func userDidProvideCreditCardInfo(cardInfo: CardIOCreditCardInfo!, inPaymentViewController paymentViewController: CardIOPaymentViewController!) {
+        if let info = cardInfo {
+            let str = NSString(format: "Received card info.\n Number: %@\n expiry: %02lu/%lu\n cvv: %@.", info.redactedCardNumber, info.expiryMonth, info.expiryYear, info.cvv)
+            statusLabel.text = str as String
+        }
+        paymentViewController?.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
+    }
+    */
+    
+}
