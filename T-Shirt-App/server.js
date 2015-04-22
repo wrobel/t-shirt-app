ae12f72ff09f3e68417f211bafbd6dc47c19ac7f
Gunnar Wrobel <p@rdus.de>
2015-04-22 19:31:06 +0200

    Write to image with random guid

--- T-Shirt-App/server.js
+++ T-Shirt-App/server.js
@@ -1,8 +1,26 @@
 var http = require('http');
 var fs = require('fs');
+
+function guid() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+    s4() + '-' + s4() + s4() + s4();
+}
+
 http.createServer(function(request, response) {
-  var destinationFile = fs.createWriteStream('image.png');
+  var tempGuid = guid(),
+      destinationFile = fs.createWriteStream(tempGuid + '.png');
   request.pipe(destinationFile);
-  response.writeHead(204, {'Content-Type': 'text/plain'});
+  response.writeHead(
+    204,
+    {
+      'Content-Type': 'text/plain',
+      'Location': tempGuid
+    }
+  );
   response.end();
 }).listen(8000);
