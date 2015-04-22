fabcc90e807e8239c2591fc6fd8cefc39dfd0d06
Gunnar Wrobel <p@rdus.de>
2015-04-22 21:40:13 +0200

    Rough dummy server part for result download

--- T-Shirt-App/server.js
+++ T-Shirt-App/server.js
@@ -1,5 +1,7 @@
 var http = require('http');
 var fs = require('fs');
+var url = require('url');
+var path = require('path');
 
 function guid() {
   function s4() {
@@ -11,16 +13,42 @@ function guid() {
     s4() + '-' + s4() + s4() + s4();
 }
 
+fs.mkdir('incoming');
+fs.mkdir('outgoing');
+
 http.createServer(function(request, response) {
-  var tempGuid = guid(),
-      destinationFile = fs.createWriteStream(tempGuid + '.png');
-  request.pipe(destinationFile);
-  response.writeHead(
-    204,
-    {
-      'Content-Type': 'text/plain',
-      'Location': tempGuid
-    }
-  );
-  response.end();
+  if (request.method === 'POST') {
+    var tempGuid = guid(),
+        destinationFile = fs.createWriteStream('incoming/' + tempGuid + '.png');
+    request.pipe(destinationFile);
+    response.writeHead(
+      204,
+      {
+        'Content-Type': 'text/plain',
+        'Location': tempGuid
+      }
+    );
+    response.end();
+  } else if (request.method === 'get') {
+    var guidPath = url.parse(request.url).pathname,
+        guid = path.normalize(guidPath).split('/')[0],
+        imagePath = 'outgoing/' + guid + '.png';
+    fs.stat(imagePath, 'r', function(err, stats) {
+      if (err) {
+        response.writeHead(404);
+        response.end();
+      } else {
+        response.writeHead(
+          200,
+          {
+            'Content-Type': 'image/png',
+            'Content-Length': stats.size
+          }
+        );
+        fs.createReadStream(imagePath).pipe(response);
+        response.end();
+      }
+    });
+    
+  }
 }).listen(8000);
