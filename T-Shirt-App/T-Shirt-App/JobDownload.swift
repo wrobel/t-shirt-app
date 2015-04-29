117a2a1e0e2db5e2a23d76f257456627e77e8637
Gunnar Wrobel <p@rdus.de>
2015-04-29 06:18:24 +0200

    Add the job download

---
+++ T-Shirt-App/T-Shirt-App/JobDownload.swift
@@ -0,0 +1,36 @@
+//
+//  JobDownload.swift
+//  T-Shirt-App
+//
+//  Created by Gunnar Wrobel on 29.04.15.
+//  Copyright (c) 2015 Gunnar Wrobel. All rights reserved.
+//
+
+import Foundation
+
+class JobDownload {
+
+    var jobDownloadedDelegate: JobDownloadedDelegate
+    
+    init(jobDownloadedDelegate delegate: JobDownloadedDelegate) {
+        jobDownloadedDelegate = delegate
+    }
+    
+    func downloadJob(queueId: String) {
+        var request = NSMutableURLRequest(URL: NSURL(string: "http://localhost:8000/" + queueId)!)
+        request.HTTPMethod = "GET"
+        let session = NSURLSession.sharedSession();
+        let task = session.dataTaskWithRequest(request, completionHandler:{
+            (data: NSData!,
+            response: NSURLResponse!,
+            error: NSError!) in
+            if (error != nil) {
+                println(error.localizedDescription)
+            } else {
+                self.jobDownloadedDelegate.jobDownloaded(UIImage(data: data)!)
+            }
+        })
+        
+        task.resume()
+    }
+}
---
+++ T-Shirt-App/T-Shirt-App/JobDownloadedProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  JobDownloadedProtocol.swift
+//  T-Shirt-App
+//
+//  Created by Gunnar Wrobel on 29.04.15.
+//  Copyright (c) 2015 Gunnar Wrobel. All rights reserved.
+//
+
+import Foundation
+
+
+protocol JobDownloadedDelegate {
+    func jobDownloaded(result: UIImage)
+}
